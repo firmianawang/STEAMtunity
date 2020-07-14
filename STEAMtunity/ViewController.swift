7dd9592066b15ece02c1d6fb342cae81816ecaa6
Firmiana Wang <firmianaw@gmail.com>
2020-07-14 14:45:17 -0500

    Initial Commit

---
+++ STEAMtunity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STEAMtunity
+//
+//  Created by Firmiana on 7/14/20.
+//  Copyright © 2020 Firmiana Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
